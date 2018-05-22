f3062b16ea9d4ff135a7318e37ff04754ffe9e56
Rochelle Lewis <rlewis37@gmail.com>
2018-05-22 14:02:50 -0600

    add base layout structure + css

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+		<!-- custom CSS -->
+		<link rel="stylesheet" href="css/style.css" />
+
+		<!-- Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+		<title>Demo Horizontal Scroll</title>
+	</head>
+	<body>
+		<div class="scroll-wrap">
+			<div class="panel">
+				<h2>Card 1</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 2</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 3</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 4</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 5</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 6</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 7</h2>
+			</div>
+			<div class="panel">
+				<h2>Card 8</h2>
+			</div>
+		</div>
+	</body>
+</html>
